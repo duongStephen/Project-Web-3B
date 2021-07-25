5b8aeb3383faada55a070a1571a36136bcc4b8f1
deathcoi <deathcoi1711@gmail.com>
2021-07-26 01:19:32 +0700

    create lai

---
+++ ThueTro/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThueTro.Models;
+using ThueTro.ViewModel;
+
+namespace ThueTro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext dbcontext;
+        public HomeController()
+        {
+            dbcontext = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var viewModel = new ModelQuan
+            {
+                DiaDiems = dbcontext.DiaDiems.ToList(),
+
+                ListHomePage = dbcontext.NhaTros
+                .Take(6).ToList()
+
+                
+            };
+            return View(viewModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
---
+++ ThueTro/Models/NhaTro.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ThueTro.Models
+{
+    public class NhaTro
+    {
+        [Key]
+        public int IDNha { get; set; }
+        [StringLength(500)]
+        
+        public string Tenduong { get; set; }
+        public string DienTich { get; set; }
+        [StringLength(50)]
+        public string TenChuNha { get; set; }
+        public string SDT { get; set; }
+        [StringLength(500)]
+        public string CTNha { get; set; }
+        [StringLength(500)]
+        public string GioiThieu { get; set; }
+        [StringLength(50)]
+        public string Gia { get; set; }
+        public string image { get; set; }
+        public string image2 { get; set; }
+        public string image3 { get; set; }
+        public int ratting { get; set; }
+        public DiaDiem diaDiem { get; set; }
+        public string IDQuann { get; set; }
+        public DateTime DateTime { get; set; }
+
+    }
+}
